721f3e2f37bdcd37be69aef97c8b522ccb8b3aec
gajul4500 <shubhamgajul310@gmail.com>
2023-11-05 15:18:35 +0530

    1

---
+++ resources/views/mailsending.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Email Title</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+    <p>This is a sample email content.</p>
+</body>
+</html>
---
+++ resources/views/pdf_template.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Email Title</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+    <p>This is a sample pdf content.</p>
+</body>
+</html>
